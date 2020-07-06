f1909b849c6424b417b9f83b29483cd3ee61ba7b
mhans19 <mdhansen19@gmail.com>
2020-07-05 18:22:21 -0600

    tested employee object

---
+++ Team-Profile-Generator/_tests_/Employee.test.js
@@ -0,0 +1,15 @@
+const Employee = require('../lib/Employee.js');
+
+test('creates an employee object', () => {
+    const employee = new Employee('Bob', "111", "test@gmail.com", "Intern");
+
+    expect(employee.name).toBe('Bob');
+    expect(employee.id).toBe("111");
+    expect(employee.email).toBe("test@gmail.com");
+    expect(employee.role).toBe("Intern");
+
+    expect(employee.getName()).toBe('Bob');
+    expect(employee.getId()).toBe('111');
+    expect(employee.getEmail()).toBe('test@gmail.com');
+    expect(employee.getRole()).toBe('Intern');
+});
